82a0bf1e847f929009d590bcdfc4b8013f9719c9
Karol <karol.kolerzak@gmail.com>
2020-06-26 13:29:01 +0200

    add validators

--- library/users/serializers.py
+++ library/users/serializers.py
@@ -4,6 +4,63 @@ from .models import User
 
 
 class RegisterUserSerializer(serializers.ModelSerializer):
+
+    def validate(self, data):
+        id_number = data['id_number']
+
+        if len(id_number) != 9:
+            raise serializers.ValidationError('ID NUMBER is too short or long')
+
+        SERIA_DATE = {
+            'A': 10,
+            'B': 11,
+            'C': 12,
+            'D': 13,
+            'E': 14,
+            'F': 15,
+            'G': 16,
+            'H': 17,
+            'I': 18,
+            'J': 20,
+            'K': 21,
+            'L': 22,
+            'M': 23,
+            'N': 24,
+            'O': 25,
+            'P': 26,
+            'R': 27,
+            'S': 28,
+            'T': 29,
+            'U': 30,
+            'W': 31,
+            'X': 32,
+            'Y': 33,
+            'Z': 34,
+        }
+
+        WEIGHT = [7, 3, 1, 7, 3, 1, 7, 3]
+
+        sum = 0
+        control_digit = int(id_number[3])
+        number = []
+        try:
+            for x in id_number[:3]:
+                number.append(SERIA_DATE[x])
+
+            number += [int(i) for i in id_number[4:]]
+
+            number = zip(number, WEIGHT)
+
+            for d, w in number:
+                sum += d * w
+        except:
+            raise serializers.ValidationError('ID NUMBER is not correct. Wrong format')
+
+        if sum % 10 == control_digit:
+            return data
+        else:
+            raise serializers.ValidationError('ID NUMBER is not correct')
+
     class Meta:
         model = User
         fields = (
